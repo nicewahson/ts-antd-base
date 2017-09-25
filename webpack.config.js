dd2114dc2b35b12ecbbe16972c0b70fe4732f7e7
nicewahson <nicelj92@gmail.com>
2017-09-25 18:07:53 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+var webpack = require('webpack')
+const tsImportPluginFactory = require('ts-import-plugin')
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        port: 3000,
+        progress: true, //报错无法识别，删除后也能正常刷新
+    },
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                options: {
+                    getCustomTransformers: () => ({
+                        before: [tsImportPluginFactory({ libraryName: "antd", style: "css" })]
+                    })
+                }
+            },
+            { test: /\.(js|jsx)$/, loader: "babel-loader" },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!less-loader"
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+};
